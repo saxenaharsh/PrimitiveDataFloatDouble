a6a02296eb918fcf10cdeeb647b7b72a6c75326a
saxenaharsh <harsh.saxena@gmail.com>
2019-06-20 19:32:48 +0530

    Add files via upload

---
+++ PrmitiveDataFloatDouble/src/com/HarshSaxena/Main.java
@@ -0,0 +1,16 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myIntNumber = 5 / 3;
+        float myFloatNumber = 5f / 3f;
+        double myDoubleNumber = 5d / 3d;
+
+        System.out.println("My int value = " + myIntNumber);
+        System.out.println("My Float value = " + myFloatNumber);
+        System.out.println("My Double value = " + myDoubleNumber);
+
+    }
+}
